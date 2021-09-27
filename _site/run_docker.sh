dc509f2bb2a198fe8e6410702641a4ba7f6f4c9d
Lars Pastewka <lars.pastewka@imtek.uni-freiburg.de>
2021-09-27 22:33:50 +0200

    Removed _site

--- _site/run_docker.sh
+++
@@ -1,1 +0,0 @@
-JEKYLL_VERSION=4.0 docker run --rm -e "JEKYLL_ENV=docker" --name Simulationstechniken --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll jekyll serve --config _config.yml,_config.docker.yml
